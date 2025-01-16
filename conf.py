1b306cab42d0ef09da9e19bc4ceb516bab3d6466
yashasvi115 <yashasvijamode0@gmail.com>
2025-01-16 20:23:29 +0530

    conf.py

---
+++ conf.py
@@ -0,0 +1,6 @@
+# Configurations for buzzer_alert.py
+
+bolt_api_key = "XXXX"                 # This is your Bolt Cloud API Key
+device_id = "XXXX"                    # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
+threshold = 40           # Threshold beyond which the buzzer will be triggered
+
